8963637d367314c961c281f45b4362fec223d4d9
thrishank <thrishankkalluru@gmail.com>
2023-08-11 23:15:02 +0530

    adding files to git

---
+++ script.js
@@ -0,0 +1,62 @@
+const dark = document.getElementById("dark");
+const quotetextelement = document.getElementById("quote-text");
+const quoteauthor = document.getElementById("quote-author");
+const updatebutton = document.getElementById("update-button");
+const quotecategory = document.getElementById("quote-category");
+const savebutton = document.getElementsByClassName("save");
+const cat1 = document.getElementsByClassName("box");
+
+dark.addEventListener("click", function () {
+  var element = document.body;
+  if (element.style.backgroundColor === "white") {
+    element.style.backgroundColor = "black";
+    element.style.color = "white";
+  } else {
+    element.style.backgroundColor = "white";
+    element.style.color = "black";
+  }
+});
+
+let url = "https://famous-quotes4.p.rapidapi.com/random?category=all&count=1";
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "63dacee26amshe3a05c6bc191c28p19ada1jsn60014b5b356d",
+    "X-RapidAPI-Host": "famous-quotes4.p.rapidapi.com",
+  },
+};
+
+cat1[0].addEventListener("click", function () {
+  url = "https://famous-quotes4.p.rapidapi.com/random?category=sports&count=1";
+  updatequote();
+});
+cat1[3].addEventListener("click", function () {
+  url = "https://famous-quotes4.p.rapidapi.com/random?category=dating&count=1";
+  updatequote();
+});
+async function fetchRandomquote() {
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("error");
+  }
+}
+async function updatequote() {
+  const quotedata = await fetchRandomquote();
+  if (quotedata) {
+    quotetextelement.innerHTML = quotedata[0].text;
+    quoteauthor.textContent = "- " + quotedata[0].author;
+    quotecategory.innerHTML = quotedata[0].category + " Quote";
+  }
+}
+updatebutton.addEventListener("click", updatequote);
+
+/* declare a async function name(){} 
+1. try - to fetch the data
+   1. store the response in a varaible using const response = await fetch(url,options)
+   2. once we got the ouptput we need to store the output in a json form so
+   3. const data = await response.json() return the data
+   4. finally incasae of an error catch(error) log error
+*/
